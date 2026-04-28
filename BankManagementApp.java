d7427dc1c94f8473e38103f3bf31fd0c04f5b22c
Ramesh krishna R <rameshkrishnartg@gmail.com>
2026-04-28 22:58:15 +0530

    Update BankManagementApp.java

--- BankManagementApp.java
+++ BankManagementApp.java
@@ -1,8 +1,5 @@
 
-package BankManagementApp;
 
-import java.sql.*;
-import java.util.Scanner;
 public class BankManagementApp {
     static Scanner scanner = new Scanner(System.in);
     private static final String DB_URL = "jdbc:mysql://localhost:3306/bank_db";
